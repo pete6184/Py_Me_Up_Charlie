8d8f9485efe2cd26efa1d45bef64ce8529989aca
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-01-07 20:21:31 -0800

    making more changes

--- Py_Bank/main.py
+++ Py_Bank/main.py
@@ -4,10 +4,9 @@ import csv
 # Path to collect data from Resources Folder
 bank_csv = os.path.join("Resources", "budget_data.csv")
 
-# Assign values for variables
-date = int[0]
-change = int[1]
-
+# Assigning default values for variables
+total_months = 0
+total_change = 0
 
 
 
@@ -20,6 +19,25 @@ with open(bank_csv, "r") as csv_file:
 
     header = next(csvreader)
 
+    # Assign values for variables
+    # date = str([0])
+    # change = int([1])
+
     # Loop through the data
-    for row in csvreader
-        
+    for row in csvreader:
+
+    # Define variables
+        total_months += 1
+        total_change = total_change + int(row[1])
+        # average_change = 
+        # greatest_increase =
+        # greatest_decrease = 
+
+        # Print out our data
+print("Financial Analysis")
+print("------------------------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_change}")
+# print(f"Average Change: ${average_change}")
+# print(f"Greatest Increase in Profits: {xxxx}")
+# print(f"Greatest Decrease in Profits: {xxxxx}")
