7402bf422f7d6b0de26b7f1dfab7c4c9401f2c34
Peter Johnson <74940976+pete6184@users.noreply.github.com>
2021-01-08 21:04:39 -0800

    stuck on finding average change and how to add months for greatest increase/decrease

--- Py_Bank/main.py
+++ Py_Bank/main.py
@@ -16,7 +16,7 @@ previous_value = 0
 pnl_change= 0
 greatest_increase = 0
 greatest_decrease = 0
-
+month_change = []
 
 # Read in the CSV file
 with open(bank_csv, "r") as csv_file:
@@ -29,26 +29,32 @@ with open(bank_csv, "r") as csv_file:
     # Loop through the data
     for row in csvreader:
 
-    # Define variables
+    # Define variable for total months
         total_months += 1
+
+    # Define variable for total profit/loss
         total_change = total_change + int(row[1])
-       
+        
+    # Define variable for average change 
         pnl_change = int(row[1]) - previous_value
         previous_value = int(row[1])
         pnl_change_list.append(pnl_change)
-
+    
+    # Find greatest month over month increase in profits
     for x in pnl_change_list:
         if x > greatest_increase:
             greatest_increase = x
-    index = pnl_change_list.index(greatest_increase)
-        
+    # index = pnl_change_list.index(greatest_increase)
+
+    # Find greatest month over month decrease in profits 
     for y in pnl_change_list:
         if y < greatest_decrease:
             greatest_decrease = y
-    index = pnl_change_list.index(greatest_decrease)
+    # index = pnl_change_list.index(greatest_decrease)
       
 
-        # Print out our data
+# Print out our data
+
 print("Financial Analysis")
 print("------------------------------------------")
 print(f"Total Months: {total_months}")
